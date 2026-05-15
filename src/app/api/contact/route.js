0d6d6367bf61b1872d79090d28a3a68f4daf3c84
Riya Kumari <riyasln783@gmail.com>
2026-05-16 00:12:27 +0530

    fixed smtp provider issue

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -1,31 +1,37 @@
 import nodemailer from "nodemailer";
 
 export async function POST(req) {
-  const body = await req.json();
+  try {
+    const body = await req.json();
 
-  const transporter = nodemailer.createTransport({
-    host: "smtppro.zoho.in",
-    port: 465,
-    secure: true,
-   auth: {
- user: process.env.EMAIL_USER,
- pass: process.env.EMAIL_PASS,
-},
-  });
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
-  await transporter.sendMail({
-    from: "info@nexora.in",
-    to: "info@nexora.in",
-    subject: "New Contact Form Message",
-    text: `
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: ["riyasln783@gmail.com"],
+      subject: "New Contact Form Message",
+      text: `
 Name: ${body.name}
 Email: ${body.email}
 Phone: ${body.phone}
 
 Message:
 ${body.message}
-    `,
-  });
+      `,
+    });
 
-  return Response.json({ success: true });
+    return Response.json({ success: true });
+  } catch (error) {
+    console.log(error);
+    return Response.json(
+      { success: false, error: error.message },
+      { status: 500 },
+    );
+  }
 }
